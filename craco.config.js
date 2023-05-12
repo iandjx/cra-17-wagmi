f7609e17d3be6af44e0691514bf589be649999f8
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2023-05-12 09:39:29 +0800

    setup wagmi

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  babel: {
+    presets: [],
+    plugins: [],
+  },
+  webpack: {
+    configure: {
+      /* webpack configuration options here */
+    },
+  },
+  devServer: {
+    /* dev server configuration options here */
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { createPublicClient, http } from "viem";
+import Profile from "./Profile";
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  }),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig config={config}>
+      <Profile />
+    </WagmiConfig>
   );
 }
 
